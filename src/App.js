08899e06f6f4364de9a485210eee1bd4fcbe9533
Shahzad6077 <44517352+Shahzad6077@users.noreply.github.com>
2020-11-22 23:45:31 +0500

    change 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Expense Tracker App Project</h1>
+        <h1>Expense Tracker App Project 2</h1>
       </header>
     </div>
   );
