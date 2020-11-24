6de01472afb7e0380b94b7583e57238169b5fbf0
shahzad6077 <alim6077@gmail.com>
2020-11-24 06:21:42 +0500

    remove unused variables

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./components.module.scss";
 
-const Header = ({}) => {
+const Header = () => {
   return (
     <header className={"container " + classes.header}>
       <h1 className="logo-font">Expense Tracker</h1>
--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,7 +1,6 @@
 import React from "react";
 import classes from "./../components.module.scss";
 import Filter from "./Filter";
-import Item from "./Item";
 import List from "./List";
 
 const History = () => {
